6dc360497259eb29cc20f815df190bacd675b33b
Jan Heinrich Reimer <heinrich@reimer.family>
2021-12-03 23:39:40 +0100

    Add example

---
+++ examples/__init__.py
@@ -0,0 +1,8 @@
+from targer.api import fetch_arguments
+
+arguments = fetch_arguments(
+    "Suicide should be a criminal offence. "
+    "It is suspected that Francis committed suicide having been "
+    "faced with being murdered over his large debt to Johnny Boy."
+)
+print(arguments)
