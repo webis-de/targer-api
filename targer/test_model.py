4e0bc4e8227234eff1c7d60798b37bd5207274c8
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-19 00:42:08 +0100

    Add model tests

--- targer/test_model.py
+++ targer/test_model.py
@@ -1,6 +1,8 @@
 from pytest import fixture
 
-from targer.model import ArgumentLabel, ArgumentTag
+from targer.model import (
+    ArgumentLabel, ArgumentTag, ArgumentSentence, ArgumentSentences
+)
 
 
 @fixture
@@ -14,6 +16,28 @@ def argument_tag(argument_label: ArgumentLabel) -> ArgumentTag:
 
 
 def test_argument_tag(argument_tag: ArgumentTag):
+    assert isinstance(argument_tag.label, ArgumentLabel)
     assert argument_tag.label == ArgumentLabel.C_I
     assert argument_tag.probability == 0.7
     assert argument_tag.token == "house"
+
+
+@fixture
+def argument_sentence(argument_tag: ArgumentTag) -> ArgumentSentence:
+    return ArgumentSentence([argument_tag])
+
+
+def test_argument_sentence(argument_sentence: ArgumentSentence):
+    assert len(argument_sentence) == 1
+    assert isinstance(argument_sentence[0], ArgumentTag)
+
+
+@fixture
+def argument_sentences(
+        argument_sentence: ArgumentSentence) -> ArgumentSentences:
+    return ArgumentSentences([argument_sentence])
+
+
+def test_argument_sentences(argument_sentences: ArgumentSentences):
+    assert len(argument_sentences) == 1
+    assert isinstance(argument_sentences[0], ArgumentSentence)
