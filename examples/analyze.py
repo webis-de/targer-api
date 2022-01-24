1f0b74c4f18530047219f204613191c8e5f9fd4a
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-24 15:41:52 +0100

    Move example

---
+++ examples/analyze.py
@@ -0,0 +1,9 @@
+from targer.api import fetch_arguments
+
+arguments = fetch_arguments(
+    "Academic freedom is not absolute. "
+    "All major Canadian universities are now publicly funded "
+    "but maintain institutional autonomy, "
+    "with the ability to decide on admission, tuition and governance."
+)
+print(arguments)
