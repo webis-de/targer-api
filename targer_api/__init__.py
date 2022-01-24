8a029f970517eadfc840a99b6def5280ba19c4f0
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-24 18:49:28 +0100

    Re-export functions and types

--- targer_api/__init__.py
+++ targer_api/__init__.py
@@ -1,2 +1,11 @@
 __version__ = "0.1.1"
 
+from targer_api import api, model
+
+# Re-export functions and types:
+analyze_text = api.analyze_text
+ArgumentLabel = model.ArgumentLabel
+ArgumentTag = model.ArgumentTag
+ArgumentSentence = model.ArgumentSentence
+ArgumentSentences = model.ArgumentSentences
+ArgumentModelSentences = model.ArgumentModelSentences
