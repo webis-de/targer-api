2beb4db3b8412710804c2cfec8ea102cd37e4946
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-24 18:41:44 +0100

    Update example

--- examples/analyze.py
+++ examples/analyze.py
@@ -7,3 +7,12 @@ arguments = analyze_text(
     "with the ability to decide on admission, tuition and governance."
 )
 print(arguments)
+
+arguments_per_model = analyze_text(
+    "Academic freedom is not absolute. "
+    "All major Canadian universities are now publicly funded "
+    "but maintain institutional autonomy, "
+    "with the ability to decide on admission, tuition and governance.",
+    {"tag-ibm-fasttext", "tag-essays-fasttext"}
+)
+print(arguments_per_model)
